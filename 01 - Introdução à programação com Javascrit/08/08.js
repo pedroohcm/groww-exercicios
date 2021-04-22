0f76d5fc81d321980d47ed3094854f2cc5b1261f
thiagobergami <thiagobergamig@gmail.com>
2021-04-22 18:53:03 -0300

    update exercicios

--- 01 - Introdução à programação com Javascrit/08/08.js
+++ 01 - Introdução à programação com Javascrit/08/08.js
@@ -1,5 +1,37 @@
 /* RESPOSTA AQUI 👇 */
 
-/* Questão 01 
+/* Questão 01
 
+Crie uma função que receba 1 número e calculo a soma de todos inteiros até ele
+exemplo:
+adicionaNumero(3)
+1 + 2 + 3 = 6
+*/
+
+/* Questão 02 
+Cria uma função que checa que compara dois arrays e retorne todos os números que estão nos dois
+
+exemplo:
+funcaoComparaArray([2,5,6], [2,7,6]) -> [2,6]
+
+Não é necessário sempre receber um array na função, pode fazer com um array estatíco
+
+*/
+
+/* Questão 03
+
+Crie uma função que receba duas números como argumento(numero, tamanho) e retorne um array
+com todos os multiplos do número, até o tamanho definido.
+exemplo:
+multiplosArray(7,5)
+retorno [7,14,21,28,35]
+
+*/
+
+/* Questão 04
+
+Crie uma função que receba um número como parametro e retorne todos os seus divisores
+
+exemplo:
+divisor(12) -> [1,2,3,4,6,12]
 */
